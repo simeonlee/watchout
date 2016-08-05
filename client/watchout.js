906531ed21c68eeca93d9b65d6d0b9141a9cbe77
Simeon Lee <simeonhlee@gmail.com>
2016-08-05 14:51:03 -0700

    Add assets and jquery file

--- client/watchout.js
+++ client/watchout.js
@@ -1,1 +1,38 @@
-// start slingin' some d3 here.
+(function() {
+
+  var options = {
+    width: $(window).width(),
+    height: $(window).height(),
+    enemies: 30
+  };
+
+  var board = d3.select('.board')
+    .append('svg')
+    .attr('width', options.width)
+    .attr('height', options.height)
+
+  board.append('circle')
+    .attr('cx', 25)
+    .attr('cy', 25)
+    .attr('r', 25)
+    .style('fill', 'white');
+
+  var data = [50, 100, 150, 200, 250];
+
+  var g = board.data(data)
+    .enter()
+    .append('div')
+    .attr('transform', 'translate(0, 10)')
+
+  var circles = g.selectAll('circle')
+    .data(data)
+
+  circles.enter()
+    .append('circle')
+    .attr({
+      cx: function(d, i) { return d; },
+      cy: 118,
+      r: 14
+    })
+
+})();
